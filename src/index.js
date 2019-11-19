1729e5ba367ef31646dae6ff37fd9e33d6656d5b
Ilya Romanyuk <ilya-romanyuk-1999@mail.ru>
2019-11-19 15:19:45 +0300

    2 домашнее задание на проверку

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
  Напишите аналог встроенного метода forEach для работы с массивами
  Посмотрите как работает forEach и повторите это поведение для массива, который будет передан в параметре array
  */
-/*function forEach(array, fn) {
+function forEach(array, fn) {
   for(let i = 0; i < array.length; i++){
       fn(array[i], i, array);
   }
@@ -22,7 +22,7 @@ function fn(item, index){
   }
 }
 
-forEach(array, fn);*/
+forEach(array, fn);
 
 /*
  Задание 2:
@@ -30,7 +30,7 @@ forEach(array, fn);*/
  Напишите аналог встроенного метода map для работы с массивами
  Посмотрите как работает map и повторите это поведение для массива, который будет передан в параметре array
  */
-/*function map(array, fn) {
+function map(array, fn) {
   let newArray = [];
   for(let i = 0; i<array.length; i++){
     newArray[i] = array[i];
@@ -47,7 +47,7 @@ let result = item * item;
 return result;
 }
 
-console.log(map(array, fn));*/
+console.log(map(array, fn));
 /*
  Задание 3:
 
@@ -55,13 +55,15 @@ console.log(map(array, fn));*/
  Посмотрите как работает reduce и повторите это поведение для массива, который будет передан в параметре array
  */
 
-/*function reduce(array, fn, initial) {
+ function reduce(array, fn, initial) {
 
   for(let i=0; i<array.length; i++){
-    if(initial === undefined){
+
+    if(initial === undefined && i === 0){
       initial = array[i];
       continue;
     }
+    
     initial = fn(initial, array[i], i, array);
   }
   return initial;
@@ -70,11 +72,12 @@ console.log(map(array, fn));*/
 let array = [1, 2, 3, 4];
 
 function fn(result, element, index, arr){
+  console.log(result);
   let sum = result + element;
+  console.log(index);
   return sum;
 }
-
-console.log(reduce(array, fn, 10))*/
+console.log(reduce(array, fn))
 
 /*
  Задание 4:
@@ -100,7 +103,60 @@ console.log(upperProps({ name: 'Сергей', lastName: 'Петров' }))
  Посмотрите как работает slice и повторите это поведение для массива, который будет передан в параметре array
  */
 function slice(array, from, to) {
+  let newArray = [];
+  for(let i = 0; i < array.length; i++){
+      if((from < to) && (from > 0) && (to > 0)){
+          for(let i = from; i < to; i++){
+              newArray.push(array[i]);
+          }
+          break;
+      }
+      else if((to === undefined) && (from > 0)){
+          for(let i = from; i < array.length; i++){
+              newArray.push(array[i]);
+          }
+          break;
+      }
+      else if(from >= 0 && to < 0){
+          let m = array.length + to;
+          for(let i = from; i < m; i++){
+              newArray.push(array[i]);
+          }
+          break
+      } 
+      else if((from < to) && (from < 0) && (to < 0)){
+          let m = array.length + to;
+          let n = array.length + from;
+          for(let i = n; i < m; i++){
+              newArray.push(array[i]);
+          }
+          break
+      }
+
+      else if((to === undefined) && (from<0)){
+          let m = array.length + from;
+          for(let i = m; i < array.length; i++){
+              newArray.push(array[i]);
+          }
+          break
+      }
+
+      else if(from === 0){
+          for(let i = from; i < array.length; i++){
+              newArray.push(array[i]);
+          }
+          break;
+      }
+
+      else if(from < 0 && to < 0 && from > to){
+          newArray = [];
+      }
+  }
+  return newArray;
 }
+
+let array = [1, 2, 3, 4, 5];
+console.log(slice(array, -2, -4));
 
 /*
  Задание 6 *:
@@ -108,8 +164,24 @@ function slice(array, from, to) {
  Функция принимает объект и должна вернуть Proxy для этого объекта
  Proxy должен перехватывать все попытки записи значений свойств и возводить это значение в квадрат
  */
+
 function createProxy(obj) {
+  let proxy = new Proxy(obj, {
+    set(target, prop, val){
+      if (typeof val === 'number') {
+        target[prop] = val * val;
+    }
+    return true;
+    }
+  });
+  return proxy
 }
+
+let obj = {}
+
+obj = createProxy(obj);
+obj.a = 2;
+console.log(obj);
 
 export {
     forEach,
